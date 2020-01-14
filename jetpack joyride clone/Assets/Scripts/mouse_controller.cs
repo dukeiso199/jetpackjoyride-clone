ec9a7f696f7b4f8a788afa76d6bbdaef80cb8447
islam dya <49913188+dukeiso199@users.noreply.github.com>
2020-01-14 20:44:18 +0200

    prototype

---
+++ jetpack joyride clone/Assets/Scripts/mouse_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_controller : MonoBehaviour
+{
+    public float jetpackForce = 75.0f;
+    private Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //    bool jetpackActive = Input.GetButton("Fire1"); mouse left click
+
+        bool Jetpackactive = Input.GetMouseButton(0);
+        if (Jetpackactive)
+        {
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+
+        }
+    }
+
+}
